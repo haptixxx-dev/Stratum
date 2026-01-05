ac09da46026313916d78b3893c4bbdbbc9af5c02
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2026-01-05 20:20:31 +0000

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,16 @@
 int main(int argc, char* argv[]) {
     spdlog::set_level(spdlog::level::debug);
     spdlog::info("Stratum v0.1.0");
-    
-    // TODO: Initialize application
-    
+
+    stratum::Application app;
+
+    if (!app.init()) {
+        spdlog::error("Failed to initialize application");
+        return 1;
+    }
+
+    app.run();
+    app.shutdown();
+
     return 0;
 }
